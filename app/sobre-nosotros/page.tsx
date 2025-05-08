2b57967f5f6e6a3256941f8c366d48d5cb64c599
gahormigag21 <gahormigag@gmail.com>
2025-05-07 20:36:46 -0500

    Creacion de paginas de about y canciones :ant:

---
+++ app/sobre-nosotros/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+    return (
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>About Us</h1>
+            <p>
+                Welcome to our application! This is the About page placeholder. More
+                information will be added here soon.
+            </p>
+        </div>
+    );
+};
+
+export default AboutPage;
